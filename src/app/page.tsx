06a9886061fc2a9664fdd0e68e6793b09a303414
sajeek0944 <sajeel6y4z@gmail.com>
2024-12-20 22:15:44 +0500

    class12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,10 +33,10 @@ export default function Home() {
 
       <div className="w-full h-[609px] bg-[#043873] px-[119px] flex gap-3 items-center [@media(max-width:1025px)]:px-12 [@media(max-width:1025px)]:h-[520px] [@media(max-width:535px)]:flex-col [@media(max-width:535px)]:px-3 [@media(max-width:535px)]:items-start [@media(max-width:535px)]:h-[750px]">
         <div className="w-[45%] h-[361px]  [@media(max-width:535px)]:w-full">
-          <h1 className="text-white font-bold text-5xl [@media(max-width:650px)]:text-4xl [@media(max-width:535px)]:pt-9">
+          <h1 className="text-white font-bold text-5xl [@media(max-width:650px)]:text-4xl [@media(max-width:535px)]:pt-9 2xl:text-6xl">
             Get More Done with whitepace
           </h1>
-          <h1 className="font-normal text-[13px] text-white py-16 [@media(max-width:1025px)]:py-10 [@media(max-width:650px)]:py-5 ">
+          <h1 className="font-normal text-[13px] text-white py-16 [@media(max-width:1025px)]:py-10 [@media(max-width:650px)]:py-5 2xl:text-lg ">
             Project management software that enables your teams to collaborate,
             plan, analyze and manage everyday tasks
           </h1>
@@ -53,11 +53,11 @@ export default function Home() {
 
       <div className="w-full h-[1350px] [@media(max-width:1025px)]:h-[1020px] [@media(max-width:535px)]:h-[1570px]">
         <div className="w-full h-[609px]  px-[119px] my-[40px] flex gap-3 items-center [@media(max-width:1025px)]:px-12 [@media(max-width:1025px)]:h-[520px] [@media(max-width:535px)]:flex-col [@media(max-width:535px)]:px-4 [@media(max-width:535px)]:h-[750px]">
-          <div className="w-[45%] h-[361px] [@media(max-width:535px)]:w-full">
-            <h1 className=" font-bold text-5xl md:text-4xl">
+          <div className="w-[45%] h-[361px] [@media(max-width:535px)]:w-full ">
+            <h1 className=" font-bold text-5xl md:text-4xl 2xl:text-7xl">
               Project Managemente
             </h1>
-            <h1 className="font-normal text-[13px]  py-16 [@media(max-width:1025px)]:py-10 [@media(max-width:535px)]:py-5">
+            <h1 className="font-normal text-[13px]  py-16 [@media(max-width:1025px)]:py-10 [@media(max-width:535px)]:py-5 2xl:text-lg">
               Images, videos, PDFs and audio files are supported. Create math
               expressions and diagrams directly from the app. Take photos with
               the mobile app and save them to a note.
@@ -80,8 +80,8 @@ export default function Home() {
           </div>
 
           <div className="w-[50%] pl-5 [@media(max-width:535px)]:w-full [@media(max-width:535px)]:pl-0 [@media(max-width:535px)]:mt-9">
-            <h1 className="font-bold text-6xl ">Work together</h1>
-            <h1 className="font-normal text-xs py-14">
+            <h1 className="font-bold text-6xl 2xl:text-7xl">Work together</h1>
+            <h1 className="font-normal text-xs py-14 2xl:text-lg">
               With whitepace, share your notes with your colleagues and
               collaborate on them. You can also publish a note to the internet
               and share the URL with others.
@@ -93,8 +93,8 @@ export default function Home() {
 
       <div className="w-full h-[609px] bg-[#043873]   px-[119px] flex gap-3 items-center [@media(max-width:1025px)]:px-12 [@media(max-width:1025px)]:h-[520px] [@media(max-width:535px)]:flex-col [@media(max-width:535px)]:h-[729px] [@media(max-width:535px)]:px-4">
         <div className="w-[45%] h-[361px] [@media(max-width:535px)]:w-full">
-          <h1 className="text-white font-bold text-5xl [@media(max-width:535px)]:pt-7">Use as Extension</h1>
-          <h1 className="font-normal text-[13px] text-white py-16 [@media(max-width:1025px)]:py-12 [@media(max-width:535px)]:py-7">
+          <h1 className="text-white font-bold text-5xl [@media(max-width:535px)]:pt-7 2xl:text-7xl">Use as Extension</h1>
+          <h1 className="font-normal text-[13px] text-white py-16 [@media(max-width:1025px)]:py-12 [@media(max-width:535px)]:py-7 2xl:text-lg">
             Use the web clipper extension, available on Chrome and Firefox, to
             save web pages or take screenshots as notes.
           </h1>
@@ -111,8 +111,8 @@ export default function Home() {
         <div className="w-[55%] h-[449px]  bg-[#C4DEFD] [@media(max-width:1025px)]:h-[350px] [@media(max-width:535px)]:w-full [@media(max-width:535px)]:mt-8"></div>
 
         <div className="w-[45%] h-[361px] [@media(max-width:1025px)]:h-[350px] [@media(max-width:535px)]:w-full">
-          <h1 className="font-bold text-5xl ">Customise it to your needs</h1>
-          <h1 className="font-normal text-[13px] py-16 [@media(max-width:1025px)]:py-10 [@media(max-width:840px)]:py-2">
+          <h1 className="font-bold text-5xl 2xl:text-7xl">Customise it to your needs</h1>
+          <h1 className="font-normal text-[13px] py-16 [@media(max-width:1025px)]:py-10 [@media(max-width:840px)]:py-2 2xl:text-lg">
             Customise the app with plugins, custom themes and multiple text
             editors (Rich Text or Markdown). Or create your own scripts and
             plugins using the Extension API.
@@ -123,10 +123,10 @@ export default function Home() {
 
       <div className="w-full h-[410px] bg-[#043873] justify-center  flex gap-3 items-center [@media(max-width:535px)]:h-[470px]">
         <div className="w-[75%] h-[301px]  text-center px-[113px] [@media(max-width:800px)]:px-12 [@media(max-width:535px)]:w-full">
-          <h1 className="text-white font-bold text-5xl [@media(max-width:840px)]:text-4xl">
+          <h1 className="text-white font-bold text-5xl [@media(max-width:840px)]:text-4xl 2xl:text-6xl">
             Your work, everywhere you are
           </h1>
-          <h1 className="font-normal text-[13px] text-white py-8 [@media(max-width:939px)]:py-1 [@media(max-width:840px)]:mt-9">
+          <h1 className="font-normal text-[13px] text-white py-8 [@media(max-width:939px)]:py-1 [@media(max-width:840px)]:mt-9 2xl:text-lg">
             Access your notes from your computer, phone or tablet by
             synchronising with various services, including whitepace, Dropbox
             and OneDrive. The app is available on Windows, macOS, Linux, Android
